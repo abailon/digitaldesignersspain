9b7470fb91a09eb500cf8fe5f5c207090a520853
abailon <alex.bailon@infojobs.net>
2017-03-21 19:24:39 +0100

    hello world

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function isEmail(email) {
+    return /(.+)@(.+){2,}\.(.+){2,}/.test(email)
+}
+
+$(function(ready){
+    var email = $('#email');
+    var signup = $('#signup');
+
+    email.on('input', function() {
+        emailValue = email.val();
+
+        if ($.trim(emailValue).length > 0 && isEmail(emailValue)) {
+            signup.removeClass("disabled");
+        } else {
+            signup.addClass("disabled");
+        }
+    });
+
+    signup.click(function() {
+        $.ajax({
+            url: 'api/signup.php',
+            type: 'post',
+            data: {email: email.val()}
+        }).done(function(data) {
+
+        });
+    });
+});
+
+//Appear effect
+$(function() {
+  $("#signup").click(function() {
+    $(".toast").addClass('appear');
+  });
+});
